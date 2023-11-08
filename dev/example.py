1290da4371ea80a9a4976e623b77365b68dfa51b
Blazej Nowicki <blazej.a.now@gmail.com>
2023-11-08 23:52:41 +0100

    debug

--- dev/example.py
+++ dev/example.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions
+import os
+
+
 
 path = "local://opt/spark/jars/"
 jars = ["bundle-2.20.18.jar", "nessie-spark-extensions-3.4_2.12-0.65.1.jar", "url-connection-client-2.20.18.jar", "iceberg-spark-runtime-3.4_2.12-1.3.0.jar", "postgresql-42.6.0.jar"]
@@ -15,23 +18,24 @@ spark = SparkSession.builder.master("k8s://https://kubernetes.default.svc.cluste
                             .config("spark.driver.port", "8002")\
                             .config("spark.driver.extraClassPath", class_path) \
                             .config("spark.blockManager.port", "8001")\
-                            .config("spark.kubernetes.namespace", "default")\
-                            .config("spark.kubernetes.container.image", "spark-executor")\
-                            .config("spark.kubernetes.container.image.pullPolicy", "Never")\
+                            .config("spark.kubernetes.namespace", os.getenv("NAMESPACE"))\
+                            .config("spark.kubernetes.container.image", "nowickib/spark-executor:0.1.0")\
+                            .config("spark.kubernetes.container.image.pullPolicy", "IfNotPresent")\
                             .config("spark.kubernetes.authenticate.driver.serviceAccountName", "spark")\
                             .config("spark.kubernetes.authenticate.executor.serviceAccountName", "spark")\
                             .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions")\
                             .config("spark.sql.catalog.nessie", "org.apache.iceberg.spark.SparkCatalog")\
                             .config("spark.sql.catalog.nessie.catalog-impl", "org.apache.iceberg.nessie.NessieCatalog")\
-                            .config("spark.sql.catalog.nessie.uri", "http://nessie:19120/api/v1")\
+                            .config("spark.sql.catalog.nessie.uri", os.getenv("NESSIE_URL"))\
                             .config("spark.sql.catalog.nessie.authentication.type", "NONE")\
                             .config("spark.sql.catalog.nessie.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")\
-                            .config("spark.sql.catalog.nessie.s3.endpoint", "http://minio:9000")\
-                            .config("spark.sql.catalog.nessie.warehouse", "s3a://warehouse")\
-                            .config("spark.sql.catalog.nessie.s3.access-key-id", "admin")\
-                            .config("spark.sql.catalog.nessie.s3.secret-access-key", "password")\
+                            .config("spark.sql.catalog.nessie.s3.endpoint", os.getenv("S3_ENDPOINT"))\
+                            .config("spark.sql.catalog.nessie.warehouse", "s3a://alice-data-lake-dev")\
+                            .config("spark.sql.catalog.nessie.s3.access-key-id", os.getenv("S3_ACCESS_KEY_ID"))\
+                            .config("spark.sql.catalog.nessie.s3.secret-access-key", os.getenv("S3_SECRET_ACCESS_KEY"))\
                             .config("spark.sql.catalog.nessie.s3.path-style-access", "true") \
-                            .config("spark.executorEnv.AWS_REGION", "eu-central-1") \
+                            .config("spark.sql.catalog.nessie.s3.region", os.getenv("S3_REGION")) \
+                            .config("spark.executorEnv.AWS_REGION", os.getenv("S3_REGION")) \
                             .config("spark.sql.defaultCatalog", "nessie")\
                             .config("spark.eventLog.enabled", "true")\
                             .config("spark.eventLog.dir", log_dir)\
