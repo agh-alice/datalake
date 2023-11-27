dac112d519df81ace881ed774c05d77c801837cc
Blazej Nowicki <blazej.a.now@gmail.com>
2023-11-27 15:18:59 +0100

    wip

--- images/airflow/dags/test_spark.py
+++ images/airflow/dags/test_spark.py
@@ -14,6 +14,6 @@ dag = DAG(
 # Define the BashOperator task
 hello_world_task = BashOperator(
     task_id='hello_world_task',
-    bash_command='echo Hello There',
+    bash_command='pyspark --version',
     dag=dag
 )
