618f753c49839dee32fd54f312a9d4a922421273
tomekl007 <tomasz.lelek@dremio.com>
2024-11-13 17:51:24 +0100

    update image

--- src/dags/utils.py
+++ src/dags/utils.py
@@ -14,7 +14,7 @@ class DatalakeFullAccessOperator(BashOperator):
             --conf spark.executor.instances={instances} \
             --conf spark.driver.extraClassPath=local://opt/spark/jars/bundle-2.20.18.jar:local://opt/spark/jars/nessie-spark-extensions-3.4_2.12-0.74.0.jar:local://opt/spark/jars/url-connection-client-2.20.18.jar:local://opt/spark/jars/iceberg-spark-runtime-3.4_2.12-1.3.0.jar:local://opt/spark/jars/postgresql-42.6.0.jar \
             --conf spark.kubernetes.namespace=datalake \
-            --conf spark.kubernetes.container.image=ghcr.io/agh-alice/spark-executor:bab6884 \
+            --conf spark.kubernetes.container.image=ghcr.io/agh-alice/spark-executor:ffde554 \
             --conf spark.kubernetes.container.image.pullPolicy=Always \
             --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
             --conf spark.kubernetes.authenticate.executor.serviceAccountName=spark \
